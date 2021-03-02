bf11310d87c010a6c199dc0e76d525610a7ee369
nattakan.p <nattakan.p@kbtg.tech>
2021-03-02 15:50:24 +0700

    UserServiceTest

--- src/main/java/com/example/kbtg/user/UserService.java
+++ src/main/java/com/example/kbtg/user/UserService.java
@@ -1,13 +1,26 @@
 package com.example.kbtg.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UserService {
+
+
+    private UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     public UserResponse getInfo(int id) {
-        if(id <= 10) {
+        Optional<MyUser> user = userRepository.findById(id);
+        if(user.isPresent()) {
             // Success
-            return new UserResponse(id, "somkiat", 30);
+            return new UserResponse(id, user.get().getName(), user.get().getAge());
         }
         // Fail
         throw new UserNotFoundException("User not found id="+ id);
